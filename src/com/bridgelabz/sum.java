a7c25036ff7590c86e13baf68ce2f15645d993c8
anuroopkanna <anuroopindia16@gmail.com>
2023-03-03 13:49:26 +0530

    added:Addition

--- src/com/bridgelabz/sum.java
+++ src/com/bridgelabz/sum.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
 public class sum {
+    public static void sum (){
+        int a=1;
+        int b=2;
+        int sum=a+b;
+        System.out.println("sum:" +sum);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome ");
+        sum();
+
     }
 }
